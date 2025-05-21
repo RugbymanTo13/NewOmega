43892aab63cafe4beac52a673be74fd15d48d5c3
RugbymanTo13 <flo.girard31@gmail.com>
2025-05-21 23:04:04 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Identifiant : omega_user_qf3v
 Mot de passe : wF2@3uV!6dPxY9#A
+
+Déployable directement sur Vercel.
---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function App() {
+  return (<div style={{padding: 40}}><h1>Bienvenue sur Omega∞</h1><p>Connexion IA activée</p></div>);
+}
+export default App;
